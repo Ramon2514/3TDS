d9573ede3b36d64c7aa98a0bd390bc4af795f224
Ramon2514 <167236520+Ramon2514@users.noreply.github.com>
2026-06-10 14:38:24 +0000

    aula sobre navegação em telas 10-06

--- tb1/app/index.tsx
+++ tb1/app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <text style={styles.titulo}>Home</text>
+      <text style={styles.subtitulo}>Bem vindo ao nosso APP</text>
+      <TouchableOpacity
+      style={styles.botao}
+      onPress={router.push('/detalhes')}
+      >
+        <text style={styles.textoBotao}>Detalhes</text>
+      </TouchableOpacity>
     </View>
   );
 }
+const styles= StyleSheet.create({
+
+})
